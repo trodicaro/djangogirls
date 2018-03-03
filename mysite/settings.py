d49744699713c5244580a185c2fe67f5b1ab14c1
Rodica Trifan <trodicaro@gmail.com>
2018-03-03 16:18:12 -0600

    View posts

--- mysite/settings.py
+++ mysite/settings.py
@@ -27,6 +27,7 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+SHELL_PLUS = 'ipython'
 
 # Application definition
 
@@ -37,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'blog'
+    'blog',
+    'django_extensions',
 ]
 
 MIDDLEWARE = [
